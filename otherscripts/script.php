7c547a50cb334ccf11c8aa468b7095a7bbafeb26
jonasfernandesnuvem <jonas.fernandes@nuvemshop.com.br>
2021-05-18 16:49:44 -0300

    .

--- otherscripts/script.php
+++ otherscripts/script.php
@@ -3,15 +3,16 @@ $servername = "mysql-balancer-eks-test.linkedstore.com";
 $username = "wpmuprod";
 $password = "mysqlwpmu4lombo";
 $dbname = "tiendanube";
-// $options = array(
-// 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-// 	PDO::MYSQL_ATTR_SSL_CA => '/home/ubuntu/rds-ca-2019-root.pem',
-// 	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-// );
+$options = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	PDO::MYSQL_ATTR_SSL_CA => '/home/ubuntu/rds-ca-2019-root.pem',
+	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+);
 
     $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $exec = $conn->query('SELECT * FROM mwp_orders WHERE store_id= "1071945"');
-    $result = $exec->fetchAll();
-    print_r($result);   
+    $result = $exec->fet
+    chAll();
+    print_r($result);
