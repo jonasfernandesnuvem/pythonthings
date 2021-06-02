beccdc6fd3be65c38c46d8bb933338c6607f0716
jonasfernandesnuvem <jonas.fernandes@nuvemshop.com.br>
2021-06-02 16:27:00 -0300

    .

--- otherscripts/2script.php
+++ otherscripts/2script.php
@@ -1,22 +1,29 @@
 <?php
-$servername = "mysql-balancer-eks-test.linkedstore.com";
-$username = "wpmuprod";
-$password = "mysqlwpmu4lombo";
-$db = "tiendanube";
+$con = mysqli_init();
+require('credentials.php')
 // Create connection
-    $mysqli -> ssl_set(NULL, NULL, NULL, NULL, NULL);
+    if (!$con){
+      die("mysqli_init failed");
+    }
+    mysqli_ssl_set($con,NULL,NULL,NULL,NULL,NULL); 
     $conn = new mysqli($servername, $username, $password,$db);
+    if (!$conn->real_connect($con,$servername, $username, $password, $db)) {
+      die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+    }
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
-        if ($result = mysqli_query($conn, "SELECT * FROM mwp_orders WHERE store_id= 1071945")) {
-        echo "Returned rows are: " . mysqli_num_rows($result);
-        while ($row = mysqli_fetch_assoc($result)) {
-            print_r($row);
-        }
-        }
-        // $result = $exec->fetchAll();
-        // print_r($result);
+    echo "Database connected";
+    printf("Client version: %d\n", mysqli_get_client_version());
+ 
+    mysqli_close($con);
+//     echo "Connected successfully";
+//         if ($result = mysqli_query($conn, "SELECT * FROM mwp_orders WHERE store_id= 1071945")) {
+//         echo "Returned rows are: " . mysqli_num_rows($result);
+//         while ($row = mysqli_fetch_assoc($result)) {
+//             print_r($row);
+//         }
+//         }
+//     mysqli_close($con);
 ?>
---
+++ otherscripts/credentials.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "mysql-balancer-eks-test.linkedstore.com";
+$username = "wpmuprod";
+$password = "mysqlwpmu4lombo";
+$db = "tiendanube";
+$con = mysqli_init();
